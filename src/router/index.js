4ae74a6b674ae6a0a5a77d94a39abea2ac3b8e8c
quangnguyen <nguyenvinhquang179@gmail.com>
2024-03-21 15:13:47 +0700

    header-frontend

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 // user
+import HomeView from '../views/HomeView.vue'
 // import UserLogin from '../views/user/LoginUserView.vue'
 import IndexUserView from '../views/user/index.vue'
-import StudentScore from '@/components/StudentScores.vue'
-import StudentInfo from '@/components/StudentInfo.vue'
 
 // ----------------------------------------------------------------
 // admin
@@ -14,20 +13,30 @@ import ListUser from '@/components/global_admin/list_user.vue'
 import AddScore from '@/components/global_admin/addscore.vue'
 import AddObject from '@/components/global_admin/add_object.vue'
 import AddNotificate from '@/components/global_admin/add_notification.vue'
+import studentscore from '@/components/StudentScores.vue'
+import List_accouts from '@/components/global_admin/list_accouts.vue'
+
 // ----------------------------------------------------------------
 // khác
 import NotFound from '@/components/NotFound.vue'
 
+
 const routes = [
   // user
   {
-    path: '/sinhvien',
+    path: '/',
     name: 'useradmin',
-    component: IndexUserView, 
+    component: IndexUserView, // Sử dụng IndexView
     children: [
-      { path: "/sinhvien/thongtin", name: 'StudentInfo', component: StudentInfo },
-      { path: "/sinhvien/diem", name: 'StudentScore', component: StudentScore },
+      { path: '/home', name: 'home', component: HomeView },// edit
+      { path: "/adduser", name: 'adduser', component: AddUser }// not found
     ]
+  },
+
+  {
+    path: '/tracuuitc',
+    name: 'studentscore',
+    component: studentscore
   },
 
     // ---------------------------------------------------------------------------------------------------------
@@ -46,7 +55,8 @@ const routes = [
     component: IndexView, // Sử dụng IndexView
     children: [
       { path: '/admin/edit/:id', name: 'edit', component: Edit },// edit
-      { path: "/admin/adduser", name: 'adduser', component: AddUser },// add
+      { path: "/admin/adduser", name: 'adduser', component: AddUser },
+      { path: "/admin/listaccout", name: 'List_accout', component: List_accouts },// add
       { path: "/admin/listuser", name: 'listuser', component: ListUser },// list
       { path: "/admin/addscore", name: 'AddScore', component: AddScore },// add điểm
       { path: "/admin/addobject", name: 'AddObject', component: AddObject },// add học phần
@@ -64,5 +74,3 @@ const router = createRouter({
 })
 
 export default router
-
-
